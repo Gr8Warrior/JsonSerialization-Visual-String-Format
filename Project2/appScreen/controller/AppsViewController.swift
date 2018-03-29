ab06dab09e8110821631e1e30e74b5ce5a59208b
shailandra-suriyal <shailandra.suriyal@hp.com>
2018-03-29 10:36:38 +0530

    Added tableView with dummy data

--- Project2/appScreen/controller/AppsViewController.swift
+++ Project2/appScreen/controller/AppsViewController.swift
@@ -6,22 +6,55 @@
 //  Copyright © 2018 intimetec. All rights reserved.
 //
 
+// UITableView <------- Array
 import UIKit
 
-class AppsViewController: UIViewController {
-
+class AppsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+  var tableOfApps: UITableView?
+  var appNames: [String]?
+  
+  func loadTableOfApps()  {
+    appNames = ["Facebook","Google","Intimetec","Apple","Yahoo","GM","Ferrari","WhatsApps"]
+    
+    var tableHeight = 0.0
+    
+    if Double((appNames?.count)!) * 50.0 > 500.0  {
+      tableHeight = 50.0
+    } else {
+      tableHeight = Double((appNames?.count)!) * 50.0
+    }
+    tableOfApps = UITableView(frame: CGRect(x: 10, y: 10, width: 300, height: tableHeight))
+    tableOfApps?.delegate = self
+    tableOfApps?.dataSource = self
+    
+    view.addSubview(tableOfApps!)
+  }
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
 
       view.backgroundColor = UIColor.blue
-        // Do any additional setup after loading the view.
+      loadTableOfApps()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return (appNames?.count)!
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
+    var cell: UITableViewCell?
+    cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+    
+    //if let guard let
+    if cell == nil {
+      cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+    }
+    cell?.textLabel?.text = appNames?[indexPath.row] //model meets view
+    //cell = UITableView(frame: CGRect(x: 10, y: 10, width: 300, height: 40))
+    return cell!
+  }
 
     /*
     // MARK: - Navigation
